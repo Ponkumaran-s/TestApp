8394bcf1ef8de64661af835f3e85609a02c5dd54
ponkumara.s <ponkumara.s@tringapps.com>
2022-11-23 11:55:08 +0530

    Intial commit

--- TestApp/ViewController.swift
+++ TestApp/ViewController.swift
@@ -8,12 +8,15 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var versionLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        if let version = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String {
+              versionLabel.text = "Version: \(version)"
+        }
     }
-
 
 }
 
